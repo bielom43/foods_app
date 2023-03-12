dac4fc66207f30ed1332b1c3623d7534b552f763
bielom43 <bielom43@gmail.com>
2023-03-12 09:06:20 -0300

    #004 add: incrementing categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -4,6 +4,22 @@ import 'package:flutter/material.dart';
 import '../components/category_item.dart';
 import '../data/dummy_data.dart';
 
+class FoodsApp extends StatelessWidget {
+  const FoodsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bem-Vindo!',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
