6ebb8b8e75542d5cd254c7a4c325790a11039a71
bielom43 <bielom43@gmail.com>
2023-03-12 11:54:08 -0300

    #008 add: customization in category itens

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -11,6 +11,18 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        ),
+      ),
       child: Text(category.title),
     );
   }
