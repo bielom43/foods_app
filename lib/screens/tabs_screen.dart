b7b9e11d1feb9c0191db03079c133944b369bf54
bielom43 <bielom43@gmail.com>
2023-03-12 18:33:28 -0300

    #025 add: create favorites screen

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,69 @@
+//packages import
+import 'package:flutter/material.dart';
+//files import
+import 'categories_screen.dart';
+import 'favorite.screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()},
+  ];
+
+  _selectedScreen(int index) {
+    setState(
+      () {
+        _selectedScreenIndex = index;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              _screens[_selectedScreenIndex]['title'] as String,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+              ),
+            ),
+          ),
+        ),
+        body: _screens[_selectedScreenIndex]['screen'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectedScreen,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          currentIndex: _selectedScreenIndex,
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favoritos',
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
