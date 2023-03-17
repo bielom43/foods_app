303999856eeb69854e73018638b7b4edef93c65e
bielom43 <bielom43@gmail.com>
2023-03-17 20:44:11 -0300

    #038 add: created settings screen and add some functionalities

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/material.dart';
 
 import '../components/main_drawer.dart';
+import '../models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
+class SettingsScreen extends StatefulWidget {
+  final Settings settings;
+  final Function(Settings) onSettingsChanged;
+
+  const SettingsScreen({
+    super.key,
+    required this.onSettingsChanged,
+    required this.settings,
+  });
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  Settings? settings;
+
+  @override
+  void initState() {
+    super.initState();
+    settings = widget.settings;
+  }
+
+  Widget _createSwitch(
+    String title,
+    String subTitle,
+    bool value,
+    Function(bool) onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subTitle),
+      value: value,
+      onChanged: (value) => {
+        onChanged(value),
+        widget.onSettingsChanged(settings!),
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +50,46 @@ class SettingsScreen extends StatelessWidget {
         title: Text('Configurações'),
       ),
       drawer: MainDrawer(),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (ctx, index) {
-          return Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: 70,
-                child: Card(
-                  child: Text('$index'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Filtros',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _createSwitch(
+                  'Sem Glúten',
+                  'Só mostra refeições sem glúten!',
+                  settings!.isGlutenFree,
+                  (value) => setState(() => settings!.isGlutenFree = value),
                 ),
-              ),
-            ],
-          );
-        },
+                _createSwitch(
+                  'Sem Lactose',
+                  'Só mostra refeições sem Lactose!',
+                  settings!.isLactoseFree,
+                  (value) => setState(() => settings!.isLactoseFree = value),
+                ),
+                _createSwitch(
+                  'Vegana',
+                  'Só mostra refeições Veganas!',
+                  settings!.isVegan,
+                  (value) => setState(() => settings!.isVegan = value),
+                ),
+                _createSwitch(
+                  'Vegetariana',
+                  'Só mostra refeições Vegetarianas!',
+                  settings!.isVegetarian,
+                  (value) => setState(() => settings!.isVegetarian = value),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
