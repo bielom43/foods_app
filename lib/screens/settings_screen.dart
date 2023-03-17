836594c8743832fc1cfa31d52e23917f43e6b1c4
bielom43 <bielom43@gmail.com>
2023-03-17 18:10:30 -0300

    #037 fix: MainDrawer created in settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/main_drawer.dart';
+
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
@@ -9,19 +11,19 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Configurações'),
       ),
+      drawer: MainDrawer(),
       body: ListView.builder(
         itemCount: 5,
         itemBuilder: (ctx, index) {
           return Column(
             children: [
               SizedBox(
-                  width: double.infinity,
-                  height: 70,
-                  child: ListTile(
-                    leading: Card(
-                      child: Text('$index'),
-                    ),
-                  )),
+                width: double.infinity,
+                height: 70,
+                child: Card(
+                  child: Text('$index'),
+                ),
+              ),
             ],
           );
         },
